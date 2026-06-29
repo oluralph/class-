027c6a8c05a0d3bb931dc926bfc39bf1bb0a3d93
ralph <olurinraphael@yahoo.co.uk>
2026-06-29 08:47:30 +0100

    adding starting project

---
+++ Class.py
@@ -0,0 +1,10 @@
+class Animal:
+    def __init__(self, animal_name, age, how_many_dozens):
+        self.name = animal_name
+        self.age = age
+        self.dozens = how_many_dozens
+    
+    def calculate_all_animal(self):
+        return self.dozens * 12
+    
+    
